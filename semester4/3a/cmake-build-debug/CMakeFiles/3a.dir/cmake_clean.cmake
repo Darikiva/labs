ff8eb0c0ec829d0cfd0c5be5b552fd1987b4f3ac
darik <darikivaa@gmail.com>
2020-05-12 22:33:27 +0300

    create

---
+++ semester4/3a/cmake-build-debug/CMakeFiles/3a.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "3a.exe"
+  "3a.pdb"
+  "CMakeFiles/3a.dir/main.cpp.o"
+  "lib3a.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
