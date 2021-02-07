5ad83cfea4ac97302f4c19f3e8b43eb49b0eb763
darik <darikivaa@gmail.com>
2021-02-07 20:42:52 +0200

    oops

--- semester6/CG/lab1/Shape.hpp
+++ semester6/CG/lab1/Shape.hpp
@@ -47,7 +47,7 @@ struct Shape
             const Point& p2 = edge.p2;
             if (p1 == point || p2 == point) return true;
             if (p1.y > point.y || p2.y < point.y) continue;
-            int num = (p1.y - p2.y) * point.x + (p2.x - p1.x) * point.y + (p1.x * p2.y - p2.x * p1.y);
+            double num = (p1.y - p2.y) * point.x + (p2.x - p1.x) * point.y + (p1.x * p2.y - p2.x * p1.y);
             if (num <= 0) ++res;
         }
         return (res % 2 != 0);
